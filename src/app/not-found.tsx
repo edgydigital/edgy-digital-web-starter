cb9868b28a423181444c73873c447279726ca44d
Karel Suchomel <karel@edgy.digital>
2023-07-31 12:16:54 +0200

    Project folder structure edit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import Button from '@/components/Button'
+import Button from '@/common/components/Button'
 import Image from 'next/image'
 import {useRouter} from 'next/navigation'
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import Logo from '@/components/Logo'
+import Logo from '@/common/components/Logo'
 
 export default function Home() {
   return (
